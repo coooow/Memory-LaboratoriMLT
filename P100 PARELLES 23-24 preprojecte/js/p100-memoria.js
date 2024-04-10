7e18c7bce3c4800507f28c2de442a5b1f21fbf2b
cow <marxfox@gmail.com>
2024-04-10 09:58:53 +0200

    mirando como hacer que salgan parejas

--- P100 PARELLES 23-24 preprojecte/js/p100-memoria.js
+++ P100 PARELLES 23-24 preprojecte/js/p100-memoria.js
@@ -1,33 +1,34 @@
 var ampladaCarta, alcadaCarta;
 var separacioH = 20, separacioV = 20;
 var nFiles = 10, nColumnes = 10;
-var f = 1, c = 1;
 let primer = true;
+var files = 4, columnes = 4;
 
 var jocCartes = [
     'carta14', 'carta1', 'carta2', 'carta3', 'carta4', 'carta5', 'carta15', 'carta16', 'carta24', 'carta25', 'carta26',
 ];
-
+var cops = [];
 
 $(function () { //cooking
-    for (let f = 1; f < 3; f++) {
-        for (let c = 1; c < 5; c++) {
+    $("#tauler").css({ //mida tauler
+        "width": 120 + 75 * columnes + "px",
+        "height": 160 + 105 * files + "px"
+    });
+    for (let f = 1; f <= files; f++) {
+        for (let c = 1; c <= columnes; c++) {
             var carta;
+            var num;
+            num = Math.floor(Math.random() * 11);
             $("#tauler").append("<div class=" + "'carta'" + " id='" + "f" + f + "c" + c + "'> <div class=" + "'cara darrera'" + "></div><div class=" + "'cara davant'" + "></div></div>");
             ampladaCarta = $(".carta").width();
             alcadaCarta = $(".carta").height();
-            // mida del tauler
-            $("#tauler").css({
-                "width": "420px", //subir de 100 en 100, 1r col = 120
-                "height": "300px" //subir de 140 en 140, 1r fila = 160
-            });
-
             carta = $("#f" + f + "c" + c);
             carta.css({
                 "left": ((c - 1) * (ampladaCarta + separacioH) + separacioH) + "px",
                 "top": ((f - 1) * (alcadaCarta + separacioV) + separacioV) + "px"
             });
-            carta.find(".davant").addClass(jocCartes.pop());
+            carta.find(".davant").addClass(jocCartes[num]);
+            jocCartes.slice(num); //AYUDA
 
         }
     }
